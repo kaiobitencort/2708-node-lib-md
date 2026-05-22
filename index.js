3d569a869a62d21025c9b435431e61ee9be75cdf
kaiobitencort <kaio.bitencort@escola.pr.gov.br>
2026-05-22 12:32:54 +0000

    trabalho 2 finalizado

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+import fs from 'fs';
 import chalk from 'chalk';
+  
+function extraiLinks(texto) {
+const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+const capturas = [...texto.matchAll(regex)];
+const resultados = capturas.map(captura => ({[captura [1]]: captura[2]}))
+return resultados;
+}
 
-console.log(chalk.yellow('Com grandes poderes vem'), chalk.black(' grandes responsabilidades.'));
+ console.log(chalk.red('com grandes poderes'), chalk.yellow('vem grandes responsabilidades') );
+ function trataErro(erro) {
+ console.log(erro);
+ throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+try {
+const encoding = 'utf-8';
+const texto = await fs.promises.readFile(caminhoDoArquivo,encoding)
+console.log(extraiLinks(texto));
+}  catch (erro) {
+trataErro(erro)
+}
+                                                             
+}
+
+// promises com then()
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.promises
+//      .readFile(caminhoDoArquivo, encoding)
+//      .then((texto) => console.log(chalk.blue(texto)))
+//      .catch(trataErro)
+//}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => { 
+//        if (erro) {
+//            trataErro(erro);
+//        }
+//     console.log(chalk.red(texto));
+//    })
+//}
+
+ pegaArquivo('./arquivos/texto.md');
+ pegaArquivo('./arquivos/');
+
+
+ // \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+ //console.log(chalk.black('preguiça'));
